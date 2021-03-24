71cb9b065e20756d84d4c860f969f92ff8944ab4
Matheus Gonçalves <matheus.goncalv_BOLS@compasso.com.br>
2021-03-24 14:33:39 -0300

    Troca de chave Fixer

--- cambio/src/main/java/br/com/compasso/cambio/model/conexao/Conecta.java
+++ cambio/src/main/java/br/com/compasso/cambio/model/conexao/Conecta.java
@@ -5,7 +5,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Conecta {
-    String link = "http://data.fixer.io/api/latest?access_key=c8d07190c602ddb54470123b47b5c554&symbols=USD,BRL,BTC&base=EUR";
+    String link = "http://data.fixer.io/api/latest?access_key=7a7f3df0f7897970a918489630acfa5e&symbols=USD,BRL,BTC&base=EUR";
 
     public HttpURLConnection conectar() throws IOException{
         URL url = new URL(link);
